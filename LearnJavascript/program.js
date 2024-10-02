84340638ff5c8f19f25641c50cc3e0db14df35de
abdul-hanan79 <abdulhanan3279@gmail.com>
2024-10-02 19:38:36 +0500

    Js variables

---
+++ LearnJavascript/program.js
@@ -0,0 +1,29 @@
+// console.log("hello world outer");
+// document.write("<h1>hello how are you?</h1>")
+
+// console.log("hello");
+
+/* console.log("hello")
+console.log("hello")
+console.log("hello")
+console.log("hello")
+console.log("hello")
+console.log("hello") */
+
+// console.log("hello");
+
+// x=1;
+// y=3;
+// x+y
+
+// numbers
+x = 123;
+x = 1.2;
+x = -1;
+
+// stirng
+x = "hello how are you";
+
+// boolean
+x = true;
+x = false;
