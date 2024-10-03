6d7b65eb548105a4e66c4f8cf037b1cb4799d7ea
abdul-hanan79 <abdulhanan3279@gmail.com>
2024-10-03 19:31:24 +0500

    js variales

--- LearnJavascript/program.js
+++ LearnJavascript/program.js
@@ -16,14 +16,74 @@ console.log("hello") */
 // y=3;
 // x+y
 
-// numbers
-x = 123;
-x = 1.2;
-x = -1;
+// // numbers
+// x = 123;
+// x = 1.2;
+// x = -1;
 
-// stirng
-x = "hello how are you";
+// // stirng
+// x = "hello how are you";
 
-// boolean
-x = true;
-x = false;
+// // boolean
+// x = true;
+// x = false;
+
+// reserved words, keywords: var, let, const,if,for,......
+
+// //  variable declaration
+// var age;
+
+// // variaable initialization
+// age = 12;
+
+// var age=12;
+
+// var money;
+// var age;
+
+// var age, money;
+// money = 56000;
+// age = 16;
+
+var username = "Abdul Hanan";
+// console.log("my name is", username);
+var present = true;
+// console.log("I'm present", present);
+var age = 16;
+// console.log("my age is", age);
+
+console.log(
+  "my name is",
+  username,
+  "my age is",
+  age,
+  "and I'm Present",
+  present
+);
+
+// Variable naming rules
+
+// 1 .Varialbe name contains (A-Z),(a-z),(0-9),_;
+// var AGE; var age, var Age,var number1, var user_name;var userName (correct)
+//  var age#,var number* , var height% (in correct)
+
+// var n1;
+
+// 2. Variable name must start with (A-Z),(a-z),_
+//  var age; var Age; var _age; (correct)
+// var 1age,var 3age, (in correct)
+// var #age;(in correct)
+
+// 3. varialbe name can't be a keyword
+// var if; vasr for, var var (in correct)
+//  var Name;var name; (case sensitive)
+
+// var name1 = "abdul hanan";
+// name1 = "ali";
+// console.log("name", name1, "name", name1);
+
+// name1 = "ahmad";
+// console.log("name", name1, "name", name1);
+
+// Problem
+// Write a js program, store your name,fathername,date of bith,age,height,class,are you hafiz? and show this in paragraph
