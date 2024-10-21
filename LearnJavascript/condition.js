6913b2df8a2719ddf17cc59b6df6e186751ba8e4
Abdul Hanan <abdulhanan3279@gmail.com>
2024-10-21 19:28:18 +0500

    if else if

--- LearnJavascript/condition.js
+++ LearnJavascript/condition.js
@@ -1,14 +1,56 @@
 // write a js program,to get the obt marks,total marks from the user and  the greate to the user
 // var n1 = 28;
-var n1 = parseInt(prompt("enter number"));
+// var n1 = parseInt(prompt("enter number"));
 // if (n1 % 2 == 0) {
 //   console.log("Number is even");
 // } else {
 //   console.log("Number is odd");
 // }
 
-if (n1 % 2 != 0) {
-  console.log("Number is Odd");
-} else {
-  console.log("Number is even");
-}
+// if (n1 % 2 != 0) {
+//   console.log("Number is Odd");
+// } else {
+//   console.log("Number is even");
+// }
+
+//  percentage >=90 , A
+//  percentage >=80 , B
+//  percentage >=60 , C
+//  percentage <60, D
+
+// var percentage=
+
+var percentage = parseInt(prompt("Enter your marks percentag"));
+
+// if (percentage >= 90) {
+//   console.log("A");
+// } else if (percentage >= 80) {
+//   console.log("B");
+// } else if (percentage >= 60) {
+//   console.log("C");
+// } else {
+//   console.log("D");
+// }
+
+// if (percentage >= 90) {
+//   console.log("A");
+// }
+
+// if (percentage >= 80) {
+//   console.log("B");
+// }
+
+// if (percentage >= 60) {
+//   console.log("C");
+// }
+
+// if (percentage < 60) {
+//   console.log("D");
+// }
+
+// write a js program , to get the values from user, if age is greater then 50 , show you are old, if age is greater than 30 show you are young, if age is greater than 18 show you are adult,otherwise show you are child
+
+//  percentage >=90 , A
+//  percentage >=80 , B
+//  percentage >=60 , C
+//  percentage <60, D
