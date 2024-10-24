0a5f34d297636b1012a0d564fe2bd598b1d27b59
Abdul Hanan <abdulhanan3279@gmail.com>
2024-10-24 19:30:16 +0500

    while loop

---
+++ LearnJavascript/loop.js
@@ -0,0 +1,37 @@
+// console.log("hello")
+
+// Type of loops
+// 1. while
+// 2. do while
+// 3. for loop
+// 4. nested loop
+
+// 1.initialization , 1
+// 2.condition <=5
+// 3.statment (work)
+// 4.increment / decrement
+
+var i = 1;
+
+while (i<10) {
+  console.log("i", i);
+  //   console.log("hello");
+  i++;
+}
+
+// console.log("i after while loop", i);
+
+
+
+/*
+2x1=2
+2x2=4
+2x3=6
+2x4=8
+2x5=10
+2x6=12
+2x7=14
+2x8=16
+2x9=18
+2x10=20
+*/
