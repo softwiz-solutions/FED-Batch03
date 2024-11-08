bcf0375b4092212c33486c28f63e483196eb34c1
Abdul Hanan <abdulhanan3279@gmail.com>
2024-11-08 18:53:37 +0500

    feat:array

---
+++ LearnJavascript/array.js
@@ -0,0 +1,30 @@
+// Data Strucuture
+
+// array.
+
+// store data in strucutred for
+
+// var student1 = "Ali";
+// var student2 = "Ahmad";
+// var student3 = "Ambar chutti vali baji";
+// var student4 = "Aqsa";
+// var student5 = "saba ";
+
+// console.log(student1);
+// console.log(student2);
+// console.log(student3);
+// console.log(student4);
+// console.log(student5);
+
+// index number
+// length , number of elements in the array
+// var studentArray = ["ali", "ahmad", "ambar", "aqsa", "saba"];
+var studentArray = [];
+studentArray[0] = "Ali";
+studentArray[1] = "Ahmad";
+studentArray[2] = "Ambar chutti vali baji";
+studentArray[3] = "Aqsa";
+studentArray[4] = "saba ";
+
+console.log("student", studentArray[0]);
+// console.log("array", studentArray[0]);
