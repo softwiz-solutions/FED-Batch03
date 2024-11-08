32e5ed4cdf031a08a414120759bf3c314aceae3a
Abdul Hanan <abdulhanan3279@gmail.com>
2024-11-08 19:21:46 +0500

    class:array

--- LearnJavascript/array.js
+++ LearnJavascript/array.js
@@ -19,12 +19,33 @@
 // index number
 // length , number of elements in the array
 // var studentArray = ["ali", "ahmad", "ambar", "aqsa", "saba"];
-var studentArray = [];
-studentArray[0] = "Ali";
-studentArray[1] = "Ahmad";
-studentArray[2] = "Ambar chutti vali baji";
-studentArray[3] = "Aqsa";
-studentArray[4] = "saba ";
+// var studentArray = [];
+// studentArray[0] = "Ali";
+// studentArray[1] = "Ahmad";
+// studentArray[2] = "Ambar chutti vali baji";
+// studentArray[3] = "Aqsa";
+// studentArray[4] = "saba ";
 
-console.log("student", studentArray[0]);
+// console.log("student", studentArray);
 // console.log("array", studentArray[0]);
+
+var fruitArray = [];
+
+for (var i = 0; i < 5; i++) {
+  fruitArray[i] = prompt("Enter fruit name");
+  //   fruitArray[0] = "Mango"
+  //   fruitArray[1] = "orange"
+  //   fruitArray[2] = "banan"
+  //   fruitArray[3] = "stwarebary"
+  //   fruitArray[4] = "peach"
+}
+
+for (var i = 0; i < fruitArray.length; i++) {
+  console.log("Array", fruitArray[i]);
+  //   console.log("Array", fruitArray[0]);
+  //   console.log("Array", fruitArray[1]);
+  //   console.log("Array", fruitArray[2]);
+  //   console.log("Array", fruitArray[3]);
+  //   console.log("Array", fruitArray[4]);
+}
+// console.log("fruit array", fruitArray.length);
