f99cfa1f872e15a90913946521bd12111a1fdb23
Abdul Hanan <abdulhanan3279@gmail.com>
2024-11-15 19:23:25 +0500

    array replacement

--- LearnJavascript/string.js
+++ LearnJavascript/string.js
@@ -1,4 +1,6 @@
-var userName = "ali";
+// var userName = "ali ahmd";
+// var replacementValue = "hello";
+// console.log("username replace", userName.replace, replacementValue));
 
 // console.log("my name is", userName.charAt(0)); //array[0]
 // console.log("my name is", userName.length); //array[0]
@@ -7,9 +9,15 @@ var userName = "ali";
 //   console.log("user name element", userName.charAt(i));
 // }
 // console.log("indedx",userName.indexOf("li"))
-
 var statement =
-  "The New Yorker magazine doesn't allow the phrase 'World War II. ' They say it should be 'the Second World War.' So let's search the following sentence for the banned characters and replace them with the phrase that the New Yorker prefers.";
-console.log("find", statement.indexOf("replace"));
-console.log("index", statement.charAt(3));
-console.log("index", statement.length);
+  "The New Yorker magazine doesn't allow the phrase 'World War II.' They say it should be 'the Second World War.' So let's search the following sentence for the banned characters and replace them with the phrase that the New Yorker prefers.";
+
+for (var i = 0; i < statement.length; i++) {
+  if (statement.slice(i, i + 12) == "World War II") {
+    statement =
+      statement.slice(0, i) + "the Second World War" + statement.slice(i + 12);
+  }
+}
+console.log("statement", statement);
+
+// write a js program, get the statement from the user, and get the word that user want to replace and also get new word, if it exists , then replace it otherwise show replacing word
