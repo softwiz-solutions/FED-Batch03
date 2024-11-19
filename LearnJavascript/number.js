480ac1c61ba274d9f5418adea0dee64fc79f1895
Abdul Hanan <abdulhanan3279@gmail.com>
2024-11-19 19:22:34 +0500

    numbers

---
+++ LearnJavascript/number.js
@@ -0,0 +1,30 @@
+// var number = 1;
+// console.log("number", Math.random()*2000);
+
+// Floor  , // ciel
+// console.log("round", Math.round(14.4)); // 14
+// console.log("floor", Math.floor(288.5));// 288
+// console.log("ciel", Math.ceil(2345.1)); // 2346
+
+// var randomNumber = Math.round(Math.random() * 1);
+
+// console.log("value", randomNumber);
+
+// if (randomNumber == 1) {
+//   alert("Tails wins");
+// } else {
+//   alert("Head wins");
+// }
+
+// var user = "24";
+// var userINNumber = Number(user);
+// console.log("user", userINNumber);
+
+// var age = 23;
+
+// console.log("age", age.toString());
+
+
+var randomNumber=23.232423435254353
+
+console.log("float",randomNumber.toFixed(3))
--- LearnJavascript/string.js
+++ LearnJavascript/string.js
@@ -9,15 +9,26 @@
 //   console.log("user name element", userName.charAt(i));
 // }
 // console.log("indedx",userName.indexOf("li"))
-var statement =
-  "The New Yorker magazine doesn't allow the phrase 'World War II.' They say it should be 'the Second World War.' So let's search the following sentence for the banned characters and replace them with the phrase that the New Yorker prefers.";
+var statement = prompt("Enter statement");
+var findingWord = prompt("Enter word");
+var replaceentWord = prompt("Enter replacement word");
+let isWordReplaced = false;
+// "The New Yorker magazine doesn't allow the phrase 'World War II.' They say it should be 'the Second World War.' So let's search the following sentence for the banned characters and replace them with the phrase that the New Yorker prefers.";
 
 for (var i = 0; i < statement.length; i++) {
-  if (statement.slice(i, i + 12) == "World War II") {
+  if (statement.slice(i, i + findingWord.length) == findingWord) {
+    isWordReplaced = true;
     statement =
-      statement.slice(0, i) + "the Second World War" + statement.slice(i + 12);
+      statement.slice(0, i) +
+      replaceentWord +
+      statement.slice(i + findingWord.length);
   }
 }
-console.log("statement", statement);
+
+if (isWordReplaced) {
+  alert("your new string is =" + statement);
+} else {
+  alert("Word not found");
+}
 
 // write a js program, get the statement from the user, and get the word that user want to replace and also get new word, if it exists , then replace it otherwise show replacing word
