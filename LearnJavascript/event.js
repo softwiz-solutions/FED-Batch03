a784ac6658653d910f813fe6a2dbc479c9d53437
Abdul Hanan <abdulhanan3279@gmail.com>
2024-12-05 19:18:04 +0500

    get input value and set input value

---
+++ LearnJavascript/event.js
@@ -0,0 +1,13 @@
+function greeting(name) {
+  alert("Hello Good Evening " + name);
+}
+
+function submit(e) {
+  //   console.log("e", e);
+  //   e.preventDefault();
+  var userEmail = document.getElementById("email").name;
+  //   userEmail.value = "Demo value";
+  console.log("🚀 ~ submit ~ userEmail:", userEmail);
+
+  //   alert("hello " + userEmail + "password" + passowrd);
+}
