abf2e3989d7314d36ef0a56d7bac07a95b5c9591
Abdul Hanan <abdulhanan3279@gmail.com>
2025-01-08 19:28:54 +0500

    dom

--- LearnJavascript/dom.js
+++ LearnJavascript/dom.js
@@ -39,14 +39,42 @@
 //   }
 // }
 
-
 // console.log("3rd para",document.childNodes[1].childNodes[1].childNodes[0].childNodes[2])
 
-function DOM(){
+function DOM() {
   // const parentNode=document.getElementById("para-div")
   // console.log("🚀 ~ DOM ~ parentNode:", parentNode.lastChild)
-  console.log("parent node",document.getElementById("2nd-child").nextSibling)
-
+  console.log("parent node", document.getElementById("2nd-child").nextSibling);
 }
-DOM()
+// DOM()
 // console.log("3rd para",document.childNodes[1].childNodes[1].childNodes[0].childNodes[2])
+function createCustomeAttribute() {
+  var para = document.getElementById("without-class");
+  // var img = document.getElementById("demo-image");
+  // console.log("id in para", para.hasAttribute("class"));
+  // console.log("id in para", img.getAttribute("src"));
+  // console.log("id in para", img.getAttribute("alt"));
+  // console.log("id in para", img.getAttribute("class"));
+  // para.setAttribute("style", "background-color:orange;color:white");
+}
+
+function createElemnent() {
+  // var div = document.getElementById("test"); // <div id="text"></div>
+  // var nodeToAdd = document.createElement("p"); //<p></p>
+  // nodeToAdd.setAttribute("class", "js-para"); //<p class="js-para"></p>
+  // var textNode = document.createTextNode("Hello this is js para"); // hello this is para
+  // nodeToAdd.appendChild(textNode); // <p class="js-para">hello this is para</p>;
+  // // nodeToAdd.innerHTML = "This i spara";
+  // div.appendChild(nodeToAdd); // <div id="test"> <p class="js-para">hello is is para</p></div>
+  var parentNode = document.getElementById("chat");
+  var headingNode = document.createElement("h1"); //<h1></h1>
+  headingNode.setAttribute("class", "demo-para"); // <h1 class="demo-para"></h1>
+  var textNode = document.createTextNode(
+    "Hello this heading is genereated by JS"
+  ); // "Hello this heading is genereated by JS"
+  // parentNode.appendChild(childNode)
+  headingNode.appendChild(textNode); // <h1 class="demo-para">Hello this heading is genereated by JS</h1>
+  parentNode.appendChild(headingNode);
+}
+
+createElemnent();
---
+++ LearnJavascript/object.js
@@ -0,0 +1,11 @@
+// var object_name={
+//     property:value
+// }
+
+var plan1 = {
+  name: "Basic", //key
+  price: 100,
+  space: 100,
+  transfer: 1000,
+  pages: 10,
+};
