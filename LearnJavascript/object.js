e0b695f643f2699c33b6f76b9a4485e5153260d9
Abdul Hanan <abdulhanan3279@gmail.com>
2025-01-09 19:31:22 +0500

    code

--- LearnJavascript/object.js
+++ LearnJavascript/object.js
@@ -2,10 +2,26 @@
 //     property:value
 // }
 
-var plan1 = {
-  name: "Basic", //key
-  price: 100,
-  space: 100,
-  transfer: 1000,
-  pages: 10,
+// var plan1 = {
+//   name: "Basic", //key
+//   price: 100,
+//   space: 100,
+//   transfer: 1000,
+//   pages: 10,
+//   benetifts: ["best option", "b2", "b3"],
+// };
+// plan1.validity = "1 year";
+// console.log("plan1", plan1.benetifts[1]);
+var student = {
+  // id: "1",
+  // name: "Aqsa",
+  // class: "9th class",
+  // subject: ["math", "english", "biology"],
+  marks: {
+    math: 19,
+    english: 14,
+    biology: 234,
+  },
 };
+
+console.log("student biology marks",student.marks.biology)
