909447f9f35d7b52f2b186ae41c6b28838d441dc
Abdul Hanan <abdulhanan3279@gmail.com>
2025-01-29 19:32:47 +0500

    React basics proper

--- LearnReact/script.jsx
+++ LearnReact/script.jsx
@@ -1,5 +1,5 @@
 // const app = document.getElementById('app');
- 
+
 // // Create a new H1 element
 // const header = document.createElement('h1');
 
@@ -13,7 +13,31 @@
 // // Place the H1 element inside the div
 // app.appendChild(header);
 
+const app = document.getElementById("app");
 
-const app = document.getElementById('app');
+function Header(props) {
+  //   console.log("propr", props.title);
+  const { title,heading } = props;
+  console.log("title");
+  //   return <h1>Develop. Pre  view. Ship.</h1>;
+  return <h1>{heading}</h1>;
+}
+
+function Para(props) {
+  const { title } = props;
+  return <p>{title}</p>;
+}
+
+function HomePage() {
+  return (
+    <div>
+      <Header heading="Heading 1" />
+      <Header heading="Heading 2" />
+      <Header heading="Heading 3" />
+      <Para title="Demo para" />
+    </div>
+  );
+}
+
 const root = ReactDOM.createRoot(app);
-root.render(<h1>Develop. Preview. Ship.</h1>);
+root.render(<HomePage />);
