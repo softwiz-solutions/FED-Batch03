4cf16d779666380ec1fcc96baf4c0b4591a8ace2
Abdul Hanan <abdulhanan3279@gmail.com>
2025-01-30 19:33:54 +0500

    Props

--- LearnReact/script.jsx
+++ LearnReact/script.jsx
@@ -17,24 +17,67 @@ const app = document.getElementById("app");
 
 function Header(props) {
   //   console.log("propr", props.title);
-  const { title,heading } = props;
+  const { title, heading } = props;
   console.log("title");
   //   return <h1>Develop. Pre  view. Ship.</h1>;
   return <h1>{heading}</h1>;
 }
 
+// function checkTitle(title) {
+//   if (title) {
+//     return title;
+//   }
+//   return "Default Title";
+// }
+
 function Para(props) {
   const { title } = props;
-  return <p>{title}</p>;
+  // console.log("title" + title);
+  // template literals
+  console.log(`Title ${title}`);
+  // return <p>{`Cool ${checkTitle(title)}`}</p>;
+
+  // 0,"",null,undefined,false
+  return <p>{`Cool ${title ? title : "defualt value"}`}</p>;
 }
 
+function List(props) {
+  const { list, color } = props;
+  console.log("🚀 ~ List ~ color:", color);
+  console.log("🚀 ~ FruitsList ~ list:", list);
+  // var fruits = ["mango", "orange", "banan", "apple"];
+
+  // fruits.map((item) => {
+  //   console.log("item", item);
+  // });
+  return (
+    <ul>
+      {/* <li>Mango</li>
+      <li>Orange</li> */}
+      {list.map((item, index) => {
+        return (
+          <li
+            className={color == "red" || index == 3 ? "list-red" : "list"}
+            key={index}
+          >
+            {item}
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
 function HomePage() {
   return (
     <div>
+      <Para title="My favourite books" />
+      <List list={["math", "eng", "urdu", "bio"]} />
       <Header heading="Heading 1" />
       <Header heading="Heading 2" />
       <Header heading="Heading 3" />
-      <Para title="Demo para" />
+      <Para />
+      <Para title="My favourite fruits" />
+      <List list={["mango", "orange", "banan", "apple"]} />
     </div>
   );
 }
