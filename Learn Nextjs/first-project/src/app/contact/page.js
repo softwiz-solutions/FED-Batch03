2650ffc62705bcd270267e17a394047ce7bcc8e5
Abdul Hanan <abdulhanan3279@gmail.com>
2025-02-07 19:07:27 +0500

    Routes

---
+++ Learn Nextjs/first-project/src/app/contact/page.js
@@ -0,0 +1,10 @@
+import Navbar from "@/components/Navbar";
+
+export default function About() {
+  return (
+    <div>
+
+      <h1> Contact page</h1>
+    </div>
+  );
+}
---
+++ Learn Nextjs/first-project/src/components/Footer.js
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer style={{ backgroundColor: "red",color:"white" }}>
+      <p>© 2025 Softwiz Solutions & Instiutte </p>
+    </footer>
+  );
+}
